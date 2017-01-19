c37f2299b3017b91557ce680d84792d9e449f3f3
Nicolas "Norswap" Laurent <norswap@gmail.com>
2017-01-19 01:21:20 +0100

    simplify the token grammar and disallow side-effects
    improves the performance of the Java parser by ~15%
    most of that seems to come from no longer copying an array when calling longest (oops)

--- src/norswap/autumn/TokenGrammar.kt
+++ src/norswap/autumn/TokenGrammar.kt
@@ -1,8 +1,6 @@
 package norswap.autumn
-import norswap.autumn.parsers.ignore_errors
-import norswap.autumn.parsers.longest
+import norswap.autumn.parsers.LongestPure
 import norswap.autumn.parsers.string
-import java.util.*
 
 // -------------------------------------------------------------------------------------------------
 
@@ -14,19 +12,22 @@ abstract class TokenGrammar: Grammar()
 {
     // ---------------------------------------------------------------------------------------------
 
-    protected open class CacheEntry (val end: Int, val type: Int, val value: Any?)
+    protected open class CacheEntry (val end: Int, val index: Int, val value: Any?)
     protected object CachedFailure: CacheEntry(-1, -1, null)
 
     // ---------------------------------------------------------------------------------------------
 
-    /** @suppress */ protected val cache       = HashMap<Int, CacheEntry>()
-    /** @suppress */ protected val parsers     = ArrayList<Parser>()
-    /** @suppress */ protected val generators  = ArrayList<TokenGenerator>()
-    /** @suppress */ protected var type_gen    = 0
-    /** @suppress */ protected val parser_array by lazy { parsers.toTypedArray() }
-
-    /** @suppress */ var max_pos  = -1
-    /** @suppress */ var max_type = -1
+    /** @suppress */ protected val cache            = HashMap<Int, CacheEntry>()
+    /** @suppress */ protected val parsers          = ArrayList<Parser>()
+    /** @suppress */ protected val generators       = ArrayList<TokenGenerator>()
+    /** @suppress */ protected var next_index       = 0
+    /** @suppress */ protected var _parser: LongestPure? = null
+    /** @suppress */ protected val parser:  LongestPure
+        get() {
+            if (_parser == null)
+                _parser = LongestPure(this, parsers.toTypedArray())
+            return _parser as LongestPure
+        }
 
     // ---------------------------------------------------------------------------------------------
 
@@ -37,44 +38,37 @@ abstract class TokenGrammar: Grammar()
 
     // ---------------------------------------------------------------------------------------------
 
-    protected inline fun token (noinline generator: TokenGenerator = { it }, crossinline p: Parser): Parser
+    protected fun token (generator: TokenGenerator = { it }, p: Parser): Parser
     {
-        val type = type_gen++
-
-        parsers.add({
-            val result = ignore_errors(p)
-            if (result && pos > max_pos) {
-                max_pos  = pos
-                max_type = type
-            }
-            result
-        })
-
+        val index = next_index++
+        parsers.add(p)
         generators.add(generator)
-        return TokenParser(intArrayOf(type))
+        return TokenParser(intArrayOf(index))
     }
 
     // ---------------------------------------------------------------------------------------------
 
-    inner class TokenParser (val types: IntArray): Parser
+    inner class TokenParser (val indices: IntArray): Parser
     {
-        override fun invoke(): Boolean {
-
+        override fun invoke(): Boolean
+        {
             val entry = cache.getOrPut(pos) {
                 var out: CacheEntry = CachedFailure
                 val pos0 = pos
-                val ptr0 = log.size
-                if (longest(*parser_array)) {
-                    val value = generators[max_type](text.substring(pos0, pos))
-                    out = CacheEntry(pos, max_type, value)
-                    max_pos  = -1
-                    max_type = -1
-                    undo(pos0, ptr0)
+                val fail_pos0 = fail_pos
+                val failure0 = failure
+                val selected = parser.select()
+                if (selected >= 0) {
+                    val value = generators[selected](text.substring(pos0, pos))
+                    out = CacheEntry(pos, selected, value)
+                    pos = pos0
                 }
+                fail_pos = fail_pos0
+                failure = failure0
                 out
             }
 
-            if (entry == CachedFailure || !types.contains(entry.type)) {
+            if (entry == CachedFailure || !indices.contains(entry.index)) {
                 return fail(pos, UnexpectedToken)
             } else {
                 pos = entry.end
@@ -101,8 +95,8 @@ abstract class TokenGrammar: Grammar()
     {
         @Suppress("UNCHECKED_CAST")
         val parsers = tokens.toList() as List<TokenParser>
-        val types = parsers.fold(intArrayOf()) { a, b -> a + b.types }
-        return TokenParser(types)
+        val indices = parsers.fold(intArrayOf()) { a, b -> a + b.indices }
+        return TokenParser(indices)
     }
 
     // ---------------------------------------------------------------------------------------------
