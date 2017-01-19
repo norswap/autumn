c09ee1d83cc29a312f2b54d77de544599baad6f4
Nicolas "Norswap" Laurent <norswap@gmail.com>
2017-01-19 01:19:31 +0100

    add Longest and LongestPure parser implementations

--- src/norswap/autumn/parsers/Choice.kt
+++ src/norswap/autumn/parsers/Choice.kt
@@ -42,3 +42,71 @@ fun Grammar.longest (vararg ps: Parser): Boolean
 }
 
 // -------------------------------------------------------------------------------------------------
+
+class Longest (val g: Grammar, val ps: Array<Parser>): Parser
+{
+    fun select(): Int
+    {
+        val pos0 = g.pos
+        val ptr0 = g.log.size
+
+        var max_pos = pos0
+        var max_delta = emptyList<Change>()
+        var max_i = -1
+
+        for (i in ps.indices ) {
+            val result = ps[i]()
+            if (result) {
+                if (g.pos > max_pos) {
+                    max_pos = g.pos
+                    max_i = i
+                    max_delta = g.diff(ptr0)
+                }
+                g.pos = pos0
+            }
+        }
+
+        if (max_i >= 0)
+            g.merge(max_pos, max_delta)
+
+        return max_i
+    }
+
+    override fun invoke(): Boolean
+    {
+        return select() >= 0
+    }
+}
+
+// -------------------------------------------------------------------------------------------------
+
+class LongestPure (val g: Grammar, val ps: Array<Parser>): Parser
+{
+    fun select(): Int
+    {
+        val pos0 = g.pos
+        var max_pos = pos0
+        var max_i = -1
+
+        for (i in ps.indices ) {
+            val result = ps[i]()
+            if (result) {
+                if (g.pos > max_pos) {
+                    max_pos = g.pos
+                    max_i = i
+                }
+                g.pos = pos0
+            }
+        }
+
+        g.pos = max_pos
+        return max_i
+    }
+
+    override fun invoke(): Boolean
+    {
+        return select() >= 0
+    }
+}
+
+// -------------------------------------------------------------------------------------------------
