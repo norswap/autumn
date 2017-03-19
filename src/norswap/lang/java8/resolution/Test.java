54fae055be6323143f5b7ce3b6f9431489687737
Nicolas "Norswap" Laurent <norswap@gmail.com>
2017-03-19 12:42:41 +0100

    remove test file

--- src/norswap/lang/java8/resolution/Test.java
+++
@@ -1,7 +0,0 @@
-package norswap.lang.java8.resolution;
-
-public class Test
-{
-    public static class Zor {}
-    public class Bob {}
-}
--- src/norswap/lang/java8/typing/Test.java
+++
@@ -1,7 +0,0 @@
-package norswap.lang.java8.typing;
-
-public class Test {
-    public void main (String[] args) {
-        short x = (short) new Byte((byte) 3);
-    }
-}
