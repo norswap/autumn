4d78d7a87fb174a5272be9e368b7225cc4347851
Nicolas Gerard <dhai@Nicolass-MacBook-Pro.local>
2017-04-20 00:29:02 +0200

    implement Look ahead naive models

--- experimental/norswap/autumn/naive/Lookahead.kt
+++ experimental/norswap/autumn/naive/Lookahead.kt
@@ -1,5 +1,9 @@
 package norswap.autumn.naive
 
+import norswap.autumn.parsers.ahead
+import norswap.autumn.parsers.ahead_pure
+import norswap.autumn.parsers.not
+
 
 // -------------------------------------------------------------------------------------------------
 
@@ -7,11 +11,9 @@ package norswap.autumn.naive
  * Succeeds if [p] succeeds, but does not advance the input position (all other side effects of
  * [p] are retained).
  */
-class ahead (val p: Parser): Parser()
+class Ahead (val p: Parser): Parser()
 {
-    override fun invoke(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun invoke() = grammar.ahead { p() }
 }
 
 // -------------------------------------------------------------------------------------------------
@@ -20,11 +22,9 @@ class ahead (val p: Parser): Parser()
  * Succeeds if [p] succeeds, but does produce any side effect (does not even change the input
  * position).
  */
-class ahead_pure (val p: Parser): Parser()
+class Ahead_pure (val p: Parser): Parser()
 {
-    override fun invoke(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun invoke() = grammar.ahead_pure { p() }
 }
 
 // -------------------------------------------------------------------------------------------------
@@ -32,11 +32,9 @@ class ahead_pure (val p: Parser): Parser()
 /**
  * Succeeds only if [p] fails.
  */
-class not (val p: Parser): Parser()
+class Not (val p: Parser): Parser()
 {
-    override fun invoke(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun invoke() = grammar.not { p() }
 }
 
 // -------------------------------------------------------------------------------------------------
