d41a342231de19839184827e2097843137c21508
Nicolas "Norswap" Laurent <norswap@gmail.com>
2018-12-10 19:07:16 +0100

    some more grammar/ast bugs

--- src/norswap/lang/java/ast/DotNew.java
+++ src/norswap/lang/java/ast/DotNew.java
@@ -3,7 +3,7 @@ package norswap.lang.java.ast;
 import com.google.auto.value.AutoValue;
 
 @AutoValue
-public abstract class DotNew
+public abstract class DotNew implements Expression
 {
     public abstract Expression operand();
     public abstract ConstructorCall constructor();
--- src/norswap/lang/java/ast/JavaFile.java
+++ src/norswap/lang/java/ast/JavaFile.java
@@ -6,12 +6,12 @@ import java.util.List;
 @AutoValue
 public abstract class JavaFile implements Statement
 {
-    public abstract PackageDeclaration package_declaration();
+    public abstract @Nullable PackageDeclaration package_declaration();
     public abstract List<ImportDeclaration> imports();
     public abstract List<TypeDeclaration> type_declarations();
 
     public static JavaFile mk (
-        PackageDeclaration package_declaration, List<ImportDeclaration> imports,
+        @Nullable PackageDeclaration package_declaration, List<ImportDeclaration> imports,
         List<TypeDeclaration> type_declarations)
     {
         return new AutoValue_JavaFile(package_declaration, imports, type_declarations);
