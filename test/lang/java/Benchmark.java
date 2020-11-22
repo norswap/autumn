8e4b3d06053cdaa5119c0718966d16bb8539471a
Nicolas "Norswap" Laurent <norswap@gmail.com>
2020-11-22 04:07:29 +0100

    improve Benchmark script & merge with the tokens version benchmark scripts

--- test/lang/java/Benchmark.java
+++ test/lang/java/Benchmark.java
@@ -1,6 +1,7 @@
 package lang.java;
 
 import norswap.autumn.Autumn;
+import norswap.autumn.DSL;
 import norswap.autumn.ParseOptions;
 import norswap.autumn.ParseResult;
 import norswap.autumn.Parser;
@@ -9,11 +10,15 @@ import norswap.autumn.TestFixture;
 import norswap.autumn.ParseMetrics;
 import norswap.lang.java.Grammar;
 import norswap.lang.java.GrammarFast;
+import norswap.lang.java.GrammarTokens;
+import norswap.lang.java.Lexer;
+import norswap.lang.java.Token;
 import norswap.utils.IO;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
@@ -31,17 +36,23 @@ public final class Benchmark
     // ---------------------------------------------------------------------------------------------
 
     private final ParseMetrics parse_metrics = new ParseMetrics();
+    private final String config;
 
     // ---------------------------------------------------------------------------------------------
 
-    public void run (String corpus_path) throws IOException
+    public Benchmark (String config) {
+        this.config = config;
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
+    public void run (String corpus_path, DSL.rule root) throws IOException
     {
-        final Grammar grammar = new Grammar();
         final List<Path> paths = IO.glob("**/*.java", Paths.get(corpus_path));
         final int slices = 100;
         final int slice_size = (paths.size() + slices - 1) / slices;
 
-        this.rule = grammar.root; // for success(input) call
+        this.rule = root; // for success(input) call
 
         long time = 0L;
         int next_slice = slice_size;
@@ -51,7 +62,7 @@ public final class Benchmark
         long size = 0;
 
         // Perform well-formed check only once!
-        Autumn.parse(grammar.root, "class Test {}", ParseOptions.get());
+        Autumn.parse(root, "class Test {}", ParseOptions.get());
 
         ParseOptions options = ParseOptions
             .well_formedness_check(false)
@@ -67,8 +78,15 @@ public final class Benchmark
             long t0 = System.nanoTime();
             String input = IO.slurp(""+ path);
             size += path.toFile().length();
+            ParseResult result;
 
-            ParseResult result = Autumn.parse(grammar.root, input, options);
+            if (config.equals("tokens")) {
+                Lexer lexer = new Lexer(input);
+                List<Token> tokens = Arrays.asList(lexer.lex());
+                 result = Autumn.parse(root, tokens, options);
+            } else {
+                result = Autumn.parse(root, input, options);
+            }
 
             time += System.nanoTime() - t0;
 
@@ -117,44 +135,37 @@ public final class Benchmark
 
     public static void main (String[] args) throws IOException
     {
-        Benchmark benchmark = new Benchmark();
+        String config = args[0];
+        String corpus_path = args[1];
+        DSL.rule root =
+            config.equals("normal")
+                ? new Grammar().root
+            : config.equals("fast")
+                ? new GrammarFast().root
+            : config.equals("tokens")
+                ? new GrammarTokens().root
+                : null;
+
+        if (root == null)
+            // TODO make a throwing function in norswap-utils and inline this in the condition above
+            throw new IllegalArgumentException("unknown benchmark config: " + config);
+
+        Benchmark benchmark = new Benchmark(config);
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             if (DO_TRACE) benchmark.pretty_print_trace();
         }));
 
-        String os = System.getProperty("os.name");
+        // NOTE(norswap): In November 2020, this run in Xs over the source of Spring 5.1.8 on my
+        // 2019 2.6Ghz MacBook Pro (to give an order of magnitude).
+        // where X:
+        // = 18.5s using Grammar
+        // = 12.5s using GrammarFast
 
-        String corpus_path =
-            args.length > 0
-                ? args[0]
-                : os.equals("Mac OS X")
-                    ? "/Users/nilaurent/Documents/bench/spring-framework-5.1.8.RELEASE"
-                    : "C:/Dropbox/bench";
-
-        // The normal Grammar (with lexical analysis) takes about 23s to run over Spring 5.1.8
-        // on MacOS.
-
-        // System.in.read();
+        // System.in.read(); // wait to attach VisualVM or some other tool
         for (int i = 0; i < iter_count; ++i)
-            benchmark.run(corpus_path);
+            benchmark.run(corpus_path, root);
     }
-
-
 
     // ---------------------------------------------------------------------------------------------
 }
-
-/* Some console commands used to run benchmarks
-
-sdk use java 19.1.1-grl
-
-for i in `seq 1 10`;
-do
-    java -cp target/classes:target/test-classes:/Users/nilaurent/.m2/repository/com/github/norswap/norswap-utils/46a351c140/norswap-utils-46a351c140.jar \
-        -Xms4g -Xmx4g lang.java.Benchmark
-done
-
-sdk use java 12.0.2-open
-
- */
